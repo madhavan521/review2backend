fde644864993335087a81c9fb7232592ffa06a48
Madhavan C <139116570+madhavan521@users.noreply.github.com>
2025-02-28 19:20:23 +0530

    Update CylinderRouter.js

--- Router/CylinderRouter.js
+++ Router/CylinderRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const ProtectRouter = require("../Middleware/ProtectRouter");
-const { postdealercylinderpurchase ,postdealerdelivery} = require("../Controller/CylinderController");
-//  createdata, getdata, deletedata, updatedata, typegetdata,
+const { postdealercylinderpurchase } = require("../Controller/CylinderController");
+//  createdata, getdata, deletedata, updatedata, typegetdata,,postdealerdelivery
 const cylinderentryrouter = express.Router();
 
 cylinderentryrouter.post('/create',ProtectRouter, postdealercylinderpurchase);
-cylinderentryrouter.post('/delivery/create' ,ProtectRouter ,postdealerdelivery);
+// cylinderentryrouter.post('/delivery/create' ,ProtectRouter ,postdealerdelivery);
 // cylinderentryrouter.get('/get',ProtectRouter, getdata);
 // cylinderentryrouter.get('/get/type', typegetdata);
 // cylinderentryrouter.put('/update/:id', ProtectRouter, updatedata);
